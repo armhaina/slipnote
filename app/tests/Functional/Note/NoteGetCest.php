86800239c9bee811d20eae236e54266fa5dd903f
Георгий Либиков <armhaina@mail.ru>
2026-01-22 01:12:35 +0300

    app

---
+++ app/tests/Functional/Note/NoteGetCest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Note;
+
+use App\DataFixtures\Note\NoteGetFixtures;
+use App\DataFixtures\Note\NoteListFixtures;
+use App\Tests\Functional\AbstractCest;
+use App\Tests\Support\FunctionalTester;
+use Codeception\Attribute\DataProvider;
+use Codeception\Example;
+use Codeception\Util\HttpCode;
+
+final class NoteGetCest extends AbstractCest
+{
+    #[DataProvider('successProvider')]
+    public function tryToTest(FunctionalTester $I, Example $example): void
+    {
+        $this->fixturesLoad(I: $I, groups: $example['groups']);
+        $this->authorized(I: $I);
+
+        $I->sendGet(url: '/api/v1/notes');
+        $I->seeResponseCodeIs(code: HttpCode::OK);
+        $I->seeResponseIsJson();
+
+        $data = json_decode($I->grabResponse(), true);
+        $data = self::except(data: $data, excludeKeys: ['id']);
+
+        $I->assertEquals(expected: $example['response'], actual: $data);
+    }
+
+    protected function successProvider(): array
+    {
+        return [
+            [
+                'groups' => NoteGetFixtures::GROUPS,
+                'response' => [
+                    'name' => 'Заметка_0',
+                    'description' => 'Описание заметки_0',
+                    'isPrivate' => false,
+                    'user' => ['email' => 'test_0@mail.ru'],
+                ],
+            ],
+        ];
+    }
+}
