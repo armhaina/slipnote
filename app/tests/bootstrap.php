78e71742b3697fa2bdd196f36ac7f91e4b308d8b
Георгий Либиков <armhaina@mail.ru>
2026-01-30 05:32:05 +0300

    app

--- app/tests/bootstrap.php
+++ app/tests/bootstrap.php
@@ -6,8 +6,8 @@ require dirname(__DIR__).'/vendor/autoload.php';
 
 if (method_exists(Dotenv::class, 'bootEnv')) {
     new Dotenv()->bootEnv(dirname(__DIR__).'/.env');
-    // Переопределить переменные из файла .env.test
-    new Dotenv()->overload(path: dirname(__DIR__).'/.env.test');
+    // Переопределить переменные из файла .env.test (deprecated)
+    // new Dotenv()->overload(path: dirname(__DIR__).'/.env.test');
 }
 
 if ($_SERVER['APP_DEBUG']) {
