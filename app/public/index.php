2cd736f38da76667c343f002e8e8574ff6b757c9
Георгий Либиков <armhaina@mail.ru>
2026-01-30 05:22:58 +0300

    app

--- app/public/index.php
+++ app/public/index.php
@@ -2,9 +2,15 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+if (file_exists(dirname(__DIR__) . '/c3.php')) {
+    require_once dirname(__DIR__) . '/c3.php';
+}
 
-include dirname(__DIR__) . '/c3.php';
+//if ($_SERVER['APP_ENV'] === 'test' && file_exists(dirname(__DIR__) . '/c3.php')) {
+//    require_once dirname(__DIR__) . '/c3.php';
+//}
+
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
