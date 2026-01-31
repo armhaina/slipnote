f29b651c273945a3899c34ddf5a113cac90c5c5a
Георгий Либиков <armhaina@mail.ru>
2026-02-01 00:37:44 +0300

    app

--- app/tests/Functional/User/UserDeleteCest.php
+++ app/tests/Functional/User/UserDeleteCest.php
@@ -7,72 +7,43 @@ namespace App\Tests\Functional\User;
 use App\Entity\User;
 use App\Tests\Functional\AbstractCest;
 use App\Tests\Support\Data\Fixture\UserFixture;
+use App\Tests\Support\Data\Trait\Test\TestFailedAuthorizationTrait;
+use App\Tests\Support\Data\Trait\Test\TestFailedForbiddenTrait;
+use App\Tests\Support\Data\Trait\Test\TestSuccessTrait;
 use App\Tests\Support\FunctionalTester;
-use Codeception\Attribute\DataProvider;
-use Codeception\Example;
-use Codeception\Util\HttpCode;
+use Symfony\Component\HttpFoundation\Request;
 
 final class UserDeleteCest extends AbstractCest
 {
+    use TestSuccessTrait;
+    use TestFailedAuthorizationTrait;
+    use TestFailedForbiddenTrait;
+
     private const string URL = '/api/v1/users';
 
-    #[DataProvider('mainProvider')]
-    public function main(FunctionalTester $I, Example $example): void
+    protected static function getMethod(): string
     {
-        $I->wantTo('DELETE/200: Удалить пользователя');
-
-        $user = $this->authorized(I: $I);
-
-        $I->sendDelete(url: self::URL.'/'.$user->getId());
-        $I->seeResponseCodeIs(code: HttpCode::OK);
-        $I->seeResponseIsJson();
-
-        $data = json_decode($I->grabResponse(), true);
-        $data = self::except(data: $data, excludeKeys: ['id']);
-
-        $I->dontSeeInRepository(entity: User::class, params: ['email' => $user->getEmail()]);
-
-        $I->assertEquals(expected: $example['response'], actual: $data);
+        return Request::METHOD_DELETE;
     }
 
-    #[DataProvider('failedAuthorizationProvider')]
-    public function failedAuthorization(FunctionalTester $I, Example $example): void
+    protected static function getUrl(FunctionalTester $I, array $context = []): string
     {
-        $I->wantTo('DELETE/401: Ошибка авторизации');
-        $user = UserFixture::load(I: $I);
+        $id = self::getEntity(I: $I, fixtures: $context['fixtures'] ?? [])->getId();
 
-        $I->sendDelete(url: self::URL.'/'.$user->getId());
-        $I->seeResponseCodeIs(code: HttpCode::UNAUTHORIZED);
-        $I->seeResponseIsJson();
-
-        $data = json_decode($I->grabResponse(), true);
-        $data = self::except(data: $data, excludeKeys: ['id']);
-
-        $I->assertEquals(expected: $example['response'], actual: $data);
+        return self::URL.'/'.$id;
     }
 
-    #[DataProvider('forbiddenProvider')]
-    public function forbidden(FunctionalTester $I, Example $example): void
-    {
-        $I->wantTo('DELETE/403: Доступ запрещен');
-
-        $this->authorized(I: $I);
-        $user = UserFixture::load(I: $I);
-
-        $I->sendDelete(url: self::URL.'/'.$user->getId());
-        $I->seeResponseCodeIs(code: HttpCode::FORBIDDEN);
-        $I->seeResponseIsJson();
-
-        $data = json_decode($I->grabResponse(), true);
-        $data = self::except(data: $data, excludeKeys: ['id']);
-
-        $I->assertEquals(expected: $example['response'], actual: $data);
-    }
-
-    protected function mainProvider(): array
+    protected function successProvider(): array
     {
         return [
             [
+                'want_to' => 'Удалить пользователя',
+                'is_authorize' => true,
+                'context' => [
+                    'fixtures' => [
+                        'email' => UserFixture::USER_AUTHORIZED_EMAIL,
+                    ],
+                ],
                 'response' => [
                     'success' => true,
                     'message' => 'Запись успешно удалена',
@@ -81,30 +52,8 @@ final class UserDeleteCest extends AbstractCest
         ];
     }
 
-    protected function failedAuthorizationProvider(): array
+    private static function getEntity(FunctionalTester $I, array $fixtures = []): User
     {
-        return [
-            [
-                'response' => [
-                    'code' => 401,
-                    'message' => 'JWT Token not found',
-                ],
-            ],
-        ];
-    }
-
-    protected function forbiddenProvider(): array
-    {
-        return [
-            [
-                'request' => [
-                    'email' => 'update@mail.ru',
-                ],
-                'response' => [
-                    'success' => false,
-                    'message' => 'Доступ запрещен',
-                ],
-            ],
-        ];
+        return UserFixture::load(I: $I, data: $fixtures);
     }
 }
